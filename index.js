6e853f9d2f996311f5494d787752e42bedb8c3af
Sanved <diwaresanved@gmail.com>
2025-08-12 17:04:02 +0530

    Post Error

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express')
 const app = express()
+const PORT=process.env.PORT||4000;
 const githubData={
   "login": "sanveddiware",
   "id": 154737109,
